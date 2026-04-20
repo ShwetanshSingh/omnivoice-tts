0b8a128f8d3ade7ef353f80139e3f72c1bc4e33c
ShwetanshSingh <contactsinghshwetansh@gmail.com>
2026-04-20 12:06:44 +0530

    feat(main.py): add code for TTS

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from omnivoice import OmniVoice
+import soundfile as sf
+import torch
+
+
 def main():
     print("Hello from omnivoice-tts!")
+    # Load the model
+    model = OmniVoice.from_pretrained(
+        "k2-fsa/OmniVoice", device_map="cuda:0", dtype=torch.float16
+    )
+
+    # Generate audio
+    audio = model.generate(
+        text="Hello, this is a test of zero-shot voice cloning.",
+        ref_audio="ref.wav",
+        ref_text="Transcription of the reference audio.",
+    )  # audio is a list of `np.ndarray` with shape (T,) at 24 kHz.
+
+    sf.write("out.wav", audio[0], 24000)
 
 
 if __name__ == "__main__":
